51926e85418501d295e99fa7d2ddc9c79976b302
unknown <eslammohamedmetwally1@gmail.com>
2024-10-29 23:08:42 +0300

    end task

--- amadeus/views.py
+++ amadeus/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .amadues_api import *
 
 def all_hotels_view(request):
+    
     city_code = request.GET.get('city_code', 'LON')
     search_query = request.GET.get('query', '')
     hotels = get_all_hotels(city_code, search_query) 
-    return render(request, 'hotels/all_hotels.html', {'hotels': hotels, 'query': search_query})
+    page_number = request.GET.get('page', 1) 
+    hotels_per_page = 10  
+    paginator = Paginator(hotels, hotels_per_page)
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'hotels/all_hotels.html',
+                {
+                'hotels': page_obj,
+                'query': search_query,
+                })
 
 
 
